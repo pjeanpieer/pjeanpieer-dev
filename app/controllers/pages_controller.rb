6c689a13775df6375bec68b1d1fdf3e9d35683e7
Pool Aguilar <jeanpieer_aguilar@hotmail.com>
2023-04-15 01:11:12 -0300

    set up css and html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download_cv
+    send_file "#{Rails.root}/public/docs/Full-stack.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get 'download_cv', to: 'pages#download_cv', as: 'download_cv'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
